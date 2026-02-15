9f771de9c56f9fabbb7cf192800302a44afc505d
Sena Zelal Demir <sedemir@k1m23s03.42kocaeli.com.tr>
2026-02-15 21:19:02 +0300

    lets go

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,11 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name)
+{
+	std::cout << "Constructor called!" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor called." << std::endl;
+}
---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string _name;
+		int			hit;
+		int			energy;
+		int			attack;
+
+	public:
+		ClapTrap(std::string name);
+		~ClapTrap();
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif
